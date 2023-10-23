5bc2ed05e53ae1ef734d2b55fa2d81417130ac25
Aditria Pardana <aditria.pardana20@gmail.com>
2023-10-23 14:31:07 +0700

    update camp section

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+interface CampProps {
+  backgroundImage: string;
+  title: string;
+  subtitle: string;
+  peopleJoined: string;
+}
+
+const CampSite = ({
+  backgroundImage,
+  title,
+  subtitle,
+  peopleJoined,
+}: CampProps) => {
+  return (
+    <div
+      className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
+    >
+      Camp site1
+    </div>
+  );
+};
 
 const Camp = () => {
   return (
-    <div>Camp</div>
-  )
-}
+    <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+        <CampSite
+          backgroundImage="bg-bg-img-1"
+          title="Putuk Truno Camp"
+          subtitle="Prigen, Pasuruan"
+          peopleJoined="50+ Joined"
+        />
+        <CampSite
+          backgroundImage="bg-bg-img-1"
+          title="Putuk Truno Camp"
+          subtitle="Prigen, Pasuruan"
+          peopleJoined="50+ Joined"
+        />
+      </div>
+    </section>
+  );
+};
 
-export default Camp
+export default Camp;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,6 @@ const Hero = () => {
     <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
       <div className="hero-map" />
 
-      {/* LEFT */}
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
         <Image
           src="/camp.svg"
@@ -15,7 +14,6 @@ const Hero = () => {
           height={50}
           className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
         />
-
         <h1 className="bold-52 lg:bold-88">Putuk Truno Camp Area</h1>
         <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
           We want to be on each of your journeys seeking the satisfaction of
@@ -29,8 +27,8 @@ const Hero = () => {
               .fill(1)
               .map((_, index) => (
                 <Image
-                  key={index}
                   src="/star.svg"
+                  key={index}
                   alt="star"
                   width={24}
                   height={24}
@@ -50,17 +48,16 @@ const Hero = () => {
           <Button type="button" title="Download App" variant="btn_green" />
           <Button
             type="button"
-            title="How we works?"
+            title="How we work?"
             icon="/play.svg"
             variant="btn_white_text"
           />
         </div>
       </div>
 
-      {/* RIGHT */}
       <div className="relative flex flex-1 items-start">
-        <div className="relative z-20 w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
-          <div className="flex flex-col mb-4">
+        <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
+          <div className="flex flex-col">
             <div className="flexBetween">
               <p className="regular-16 text-gray-20">Location</p>
               <Image src="/close.svg" alt="close" width={24} height={24} />
@@ -75,7 +72,7 @@ const Hero = () => {
             </div>
             <div className="flex flex-col">
               <p className="regular-16 block text-gray-20">Elevation</p>
-              <p className="bold-20 text-white">2 km</p>
+              <p className="bold-20 text-white">2.040 km</p>
             </div>
           </div>
         </div>
