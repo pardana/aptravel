016d0460ad76fd668d0069126af9be531c1d3eb7
Aditria Pardana <aditria.pardana20@gmail.com>
2023-10-24 16:39:34 +0700

    update footer and logo

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 const GetApp = () => {
   return (
-    <section className="flexCenter w-full pb-[100px]">
+    <section className="flexCenter w-full pb-[50px]">
       <div className="get-app">
         <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
           <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,10 @@ const Navbar = () => {
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
-        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+        {/* <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} /> */}
+        <p className="bold-32">
+          AP<span className="text-green-50">Travel.</span>
+        </p>
       </Link>
 
       <ul className="hidden h-full gap-12 lg:flex">
@@ -29,6 +32,7 @@ const Navbar = () => {
           title="Login"
           icon="/user.svg"
           variant="btn_dark_green"
+          full={false}
         />
       </div>
 
