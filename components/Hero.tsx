f76ab9d8994cf151066065f3d949cd7304ecccc3
Aditria Pardana <aditria.pardana20@gmail.com>
2023-10-24 16:56:45 +0700

    update button in Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -45,12 +45,19 @@ const Hero = () => {
         </div>
 
         <div className="flex flex-col w-full gap-3 sm:flex-row">
-          <Button type="button" title="Download App" variant="btn_green" />
+          <Button
+            type="button"
+            title="Download App"
+            icon=""
+            variant="btn_green"
+            full={false}
+          />
           <Button
             type="button"
             title="How we work?"
             icon="/play.svg"
             variant="btn_white_text"
+            full={false}
           />
         </div>
       </div>
